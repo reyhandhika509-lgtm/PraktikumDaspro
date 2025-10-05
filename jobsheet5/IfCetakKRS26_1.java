821e89f3d210656697991c64e2f4d22666921ea7
reyhandhika509-lgtm <reyhandhika509@gmail.com>
2025-10-05 18:43:59 +0700

    Tugas 1

---
+++ jobsheet5/IfCetakKRS26_1.java
@@ -0,0 +1,23 @@
+package jobsheet5;
+
+import java.util.Scanner;
+
+public class IfCetakKRS26_1 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+      System.out.println("---Cetak KRS SIAKAD ---");
+        System.out.print("Apakah UKT sudah Lunas? (True/false): ");
+        boolean uktlunas = sc.nextBoolean();
+        if (uktlunas) {
+            System.out.println(" pembayaran UKT terverifikasi");
+            System.out.println("Silahkan cetak KSR dan minta tanda tangan DPA");
+           } else {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu.");
+
+        sc.close(); 
+
+        }
+    }
+}
+
