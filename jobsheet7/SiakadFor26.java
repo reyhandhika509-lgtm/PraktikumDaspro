eb242435cfa9382b9a5263a202b9e24de141c7f0
reyhandhika509-lgtm <reyhandhika509@gmail.com>
2025-10-21 11:50:21 +0700

    Modifikasi Percobaan 1

--- jobsheet7/SiakadFor26.java
+++ jobsheet7/SiakadFor26.java
@@ -7,6 +7,7 @@ public class SiakadFor26 {
         Scanner sc = new Scanner(System.in);
         
         double nilai, tertinggi = 0, terendah =100;
+        int lulus = 0, tidaklulus=0;
         
         for (int i = 1 ; i <=10; i++){
             System.out.print("Masukkan Nilai Mahasiswa ke-" + i + ":");
@@ -16,10 +17,18 @@ public class SiakadFor26 {
             }
             if (nilai < terendah) {
                 terendah = nilai;
-            }  
+            } 
+            if (nilai >= 60) {
+                lulus++ ;
+                
+            } else {
+                tidaklulus++;
+            }
         }
         System.out.println("Nilai Tertinggi :" + tertinggi);
-        System.out.println("Nilai Terendah :"+ terendah);
+        System.out.println("Nilai Terendah :"+ terendah); 
+        System.out.println("Jumlah Siswa yang lulus: " + lulus);
+        System.out.println("Jumlah Siswa yang Tidak lulus:" + tidaklulus);
 
         sc.close();
     }
