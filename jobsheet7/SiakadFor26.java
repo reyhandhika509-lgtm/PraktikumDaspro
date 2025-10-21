39ef83b7093dc7b01027cee1d2653b8746690af2
reyhandhika509-lgtm <reyhandhika509@gmail.com>
2025-10-21 11:15:23 +0700

    percobaan 1

---
+++ jobsheet7/SiakadFor26.java
@@ -0,0 +1,26 @@
+package jobsheet7;
+
+import java.util.Scanner;
+
+public class SiakadFor26 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        double nilai, tertinggi = 0, terendah =100;
+        
+        for (int i = 1 ; i <=10; i++){
+            System.out.print("Masukkan Nilai Mahasiswa ke-" + i + ":");
+            nilai = sc.nextDouble();
+            if (nilai > tertinggi) {
+                tertinggi = nilai;
+            }
+            if (nilai < terendah) {
+                terendah = nilai;
+            }  
+        }
+        System.out.println("Nilai Tertinggi :" + tertinggi);
+        System.out.println("Nilai Terendah :"+ terendah);
+        
+    }
+    
+}
