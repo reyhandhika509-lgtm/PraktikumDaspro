1748ffa9b09995677b5b980bb622c118b9df8101
reyhandhika509-lgtm <reyhandhika509@gmail.com>
2025-10-29 13:01:36 +0700

    Percobaan 1

---
+++ jobsheet8/Star26.java
@@ -0,0 +1,17 @@
+package jobsheet8;
+
+import java.util.Scanner;
+
+public class Star26 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print(" Masukkan Nilai n =");
+        int n =sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+            System.out.print("*");
+        }
+    }
+    
+}
