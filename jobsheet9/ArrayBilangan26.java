14b1c71a89c729ca95ee9feed43ca4f78f582bc6
reyhandhika509-lgtm <reyhandhika509@gmail.com>
2025-11-04 10:20:13 +0700

    Modifikasi Percobaan 1

--- jobsheet9/ArrayBilangan26.java
+++ jobsheet9/ArrayBilangan26.java
@@ -5,7 +5,7 @@ public class ArrayBilangan26 {
         int [] bil ={ 5, 13, -7, 17};
 
         for (int i = 0 ; i < 4; i++){
-            System.out.println(bil[1]);
+            System.out.println(bil[i]);
         }   
     }
 }
