977ea649891d9f524e4c6a0462b4fd0b1dd4dcae
reyhandhika509-lgtm <reyhandhika509@gmail.com>
2025-11-07 17:55:07 +0700

    Modifikasi Percobaan 4

--- jobsheet9/SearchNilai26.java
+++ jobsheet9/SearchNilai26.java
@@ -1,20 +1,41 @@
 package jobsheet9;
 
+import java.util.Scanner;
+
 public class SearchNilai26 {
     public static void main(String[] args) {
-        int [] arrnilai = {80, 85, 78, 96, 90, 82, 86};
-        int key  = 90;
-        int hasil = 0;
+         Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < arrnilai.length; i++ ){
-            if(key == arrnilai[i]){
-                hasil = i ;
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int jumlah = sc.nextInt();
+        int[] arrnilai = new int[jumlah];
+
+        // input elemen array
+        for (int i = 0; i < arrnilai.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            arrnilai[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = sc.nextInt();
+        int hasil = -1;
+
+        // proses pencarian
+        for (int i = 0; i < arrnilai.length; i++) {
+            if (key == arrnilai[i]) {
+                hasil = i;
                 break;
             }
         }
-        System.out.println();
-        System.out.println("Nilai " +key+ " ketemu di indeks ke-"+hasil);
-        System.out.println();
 
+        System.out.println();
+        if (hasil != -1) {
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil + 1));
+        } else {
+            System.out.println("Nilai " + key + " tidak ditemukan dalam data.");
+        }
+
+        System.out.println();
+        
     }
 }
