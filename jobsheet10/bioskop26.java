f7d4011a82ad35f7ecce55e0225e2f4cd3de0786
reyhandhika509-lgtm <reyhandhika509@gmail.com>
2025-11-11 10:08:09 +0700

    Modifikasi Percobaan 1

--- jobsheet10/bioskop26.java
+++ jobsheet10/bioskop26.java
@@ -11,11 +11,46 @@ public class bioskop26 {
         penonton[2][0] ="Eka";
         penonton[2][1] ="Farhan";
         penonton[3][0] ="Gisel";
+        penonton[3][1] ="Hana";
 
         System.out.printf("%s\t %s\n", penonton[0][0], penonton[0][1]);
         System.out.printf("%s\t %s\n", penonton[1][0], penonton[1][1]);
         System.out.printf("%s\t %s\n", penonton[2][0], penonton[2][1]);
-        System.out.printf("%s\t %s\n", penonton[3][0], penonton[3][1]);
+        System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+        
+        System.out.println();
+
+        // System.out.println(penonton.length);
+        // System.out.println(penonton[0].length);
+        // System.out.println(penonton[1].length);
+        // System.out.println(penonton[2].length);
+        // System.out.println(penonton[3].length);
+        
+        // System.out.println(penonton.length);
+        // for (int i = 0; i <penonton.length; i++){
+        //     System.out.println("Panjang  baris ke-" + (i+1) + ":" + penonton[i].length);
+        // }
+
+        // System.out.println(penonton.length);
+
+        // for (String[] barispenonton : penonton){
+        //     System.out.println("Panjang Baris: " + barispenonton.length);
+        // }
+
+        // System.out.println("Penonton pada baris ke-3: ");
+
+        // for (int i = 0; i < penonton[2].length; i++){
+        //     System.out.println(penonton[2][i]);
+        // }
+
+        for (int i = 0; i < penonton.length; i++){
+            System.out.println("Penonton Pada Baris Ke-" + (i+1) + ": " + String.join(",", penonton[i]));
+        }
+
+        System.out.println("Penonton Pada Baris Ke-3: ");
+
+        for (String i : penonton[2]){
+            System.out.println(i);
+        }
     }
-    
 }
