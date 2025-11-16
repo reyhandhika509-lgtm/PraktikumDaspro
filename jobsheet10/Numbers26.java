4900fdef88070e1e54242e9e4e6f167f43eaa967
reyhandhika509-lgtm <reyhandhika509@gmail.com>
2025-11-16 12:54:07 +0700

    Mofifikasi Percobaan 3

--- jobsheet10/Numbers26.java
+++ jobsheet10/Numbers26.java
@@ -1,5 +1,7 @@
 package jobsheet10;
 
+import java.util.Arrays;
+
 public class Numbers26 {
     public static void main(String[] args) {
         
@@ -7,6 +9,12 @@ public class Numbers26 {
         myNumbers[0] = new int[5];
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
-
+        
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Panjang baris ke-" + (i+1) + ": " + myNumbers[i].length);
+        }
     }
 }
