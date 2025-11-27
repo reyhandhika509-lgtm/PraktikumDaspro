47d074e4d11c0c1501f7ed743b0125d8aee2c825
reyhandhika509-lgtm <reyhandhika509@gmail.com>
2025-11-27 07:45:32 +0700

    Percobaan 1

---
+++ Jobsheet11/Kafe26.java
@@ -0,0 +1,19 @@
+package Jobsheet11;
+
+public class Kafe26 {
+    public static void menu() {
+        System.out.println("==== MENU RESTO KAFE ====");
+        System.out.println("1. KOPI HITAM - Rp 15.000"); 
+        System.out.println("2. CAPPUCCINO - Rp 20.000"); 
+        System.out.println("3. LATTE - Rp 22.000");  
+        System.out.println("4. TEH TARIK - Rp 12.000");
+        System.out.println("5. ROTI BAKAR - Rp 10.000");
+        System.out.println("6. MIE GORENG - Rp 18.000");
+        System.out.println("=========================");   
+        System.out.println("Silahkan pilih menu yang anda inginkan");
+
+    }
+    public static void main(String[] args) {
+        menu();
+    }
+}
